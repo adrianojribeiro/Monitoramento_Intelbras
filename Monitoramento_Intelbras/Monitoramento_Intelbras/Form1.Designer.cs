2bc30a23e7b94633b837a8d543b2d88161280822
adrianojribeiro <adriano.jribeiro1081@gmail.com>
2020-04-28 20:24:51 -0300

    Envio Inicial

---
+++ Monitoramento_Intelbras/Monitoramento_Intelbras/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace Monitoramento_Intelbras
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axVLCPlugin21 = new AxAXVLC.AxVLCPlugin2();
+            this.btnstart = new System.Windows.Forms.Button();
+            this.txtcamera = new System.Windows.Forms.TextBox();
+            this.btnstop = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.axVLCPlugin21)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axVLCPlugin21
+            // 
+            this.axVLCPlugin21.Enabled = true;
+            this.axVLCPlugin21.Location = new System.Drawing.Point(12, 12);
+            this.axVLCPlugin21.Name = "axVLCPlugin21";
+            this.axVLCPlugin21.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axVLCPlugin21.OcxState")));
+            this.axVLCPlugin21.Size = new System.Drawing.Size(730, 347);
+            this.axVLCPlugin21.TabIndex = 0;
+            // 
+            // btnstart
+            // 
+            this.btnstart.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnstart.Location = new System.Drawing.Point(12, 445);
+            this.btnstart.Name = "btnstart";
+            this.btnstart.Size = new System.Drawing.Size(108, 35);
+            this.btnstart.TabIndex = 1;
+            this.btnstart.Text = "Start";
+            this.btnstart.UseVisualStyleBackColor = true;
+            this.btnstart.Click += new System.EventHandler(this.btnstart_Click);
+            // 
+            // txtcamera
+            // 
+            this.txtcamera.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtcamera.Location = new System.Drawing.Point(126, 447);
+            this.txtcamera.Name = "txtcamera";
+            this.txtcamera.Size = new System.Drawing.Size(100, 30);
+            this.txtcamera.TabIndex = 2;
+            // 
+            // btnstop
+            // 
+            this.btnstop.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnstop.Location = new System.Drawing.Point(12, 486);
+            this.btnstop.Name = "btnstop";
+            this.btnstop.Size = new System.Drawing.Size(108, 35);
+            this.btnstop.TabIndex = 3;
+            this.btnstop.Text = "Stop";
+            this.btnstop.UseVisualStyleBackColor = true;
+            this.btnstop.Click += new System.EventHandler(this.btnstop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1016, 533);
+            this.Controls.Add(this.btnstop);
+            this.Controls.Add(this.txtcamera);
+            this.Controls.Add(this.btnstart);
+            this.Controls.Add(this.axVLCPlugin21);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.axVLCPlugin21)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private AxAXVLC.AxVLCPlugin2 axVLCPlugin21;
+        private System.Windows.Forms.Button btnstart;
+        private System.Windows.Forms.TextBox txtcamera;
+        private System.Windows.Forms.Button btnstop;
+    }
+}
+
---
+++ Monitoramento_Intelbras/Monitoramento_Intelbras/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Monitoramento_Intelbras
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnstart_Click(object sender, EventArgs e)
+        {
+            Carrega_Camera();
+        }
+
+        public void Carrega_Camera()
+        {
+            try
+            {
+                int camera = Convert.ToInt32(txtcamera.Text);
+                // string _rtspEndpoint = "rtsp://192.168.1.88:554/user=admin&password=g5a7b9t3&channel=1&stream=0.sdp?";
+
+                string _rtspEndpoint = "rtsp://admin:g5a7b9t3@192.168.1.102:554/cam/realmonitor?channel=" + camera + "&subtype=0";
+                axVLCPlugin21.playlist.add(_rtspEndpoint);
+                axVLCPlugin21.playlist.play();             
+            }
+            catch (Exception erro)
+            {
+                MessageBox.Show(erro.ToString());
+            }
+        }
+
+        private void btnstop_Click(object sender, EventArgs e)
+        {
+            axVLCPlugin21.playlist.stop_async();
+            axVLCPlugin21.playlist.stop();      
+
+        }
+    }
+}
